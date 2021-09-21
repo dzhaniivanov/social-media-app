ae4055131f4e45f9b13f2202d1c3cacc43d6930f
dzhani ivanov <djani.ivanov94@gmail.com>
2021-09-21 15:30:24 +0300

    added put request

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,31 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
-router.get("/", (req, res) => {
-    res.send("user routes")
+
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.user.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (error) {
+                return res.status(500).json(error);
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("Account has been updated")
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        return res.status(403).json("You can update only your account")
+    }
 });
+
 
 
 module.exports = router;
