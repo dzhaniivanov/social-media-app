fedf07ee1142ea989fdd4d1144a4e3acd398828a
dzhani ivanov <djani.ivanov94@gmail.com>
2021-09-21 12:42:12 +0300

    post request for register

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-    res.send("auth routes")
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    try {
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 
