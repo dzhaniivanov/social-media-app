f4fdba86469e6af26c82ae5f447b6340bbfce0e8
dzhani ivanov <djani.ivanov94@gmail.com>
2021-09-24 17:34:31 +0300

    add route to get all posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -54,6 +54,30 @@ router.put("/:id/like", async (req, res) => {
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId });
+            })
+        );
+        res.json(userPosts.concat(...friendPosts))
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
