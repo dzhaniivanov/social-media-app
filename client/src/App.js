2ebce8761a7ee4ceb074f60aecc3f7b304741f17
dzhani ivanov <djani.ivanov94@gmail.com>
2021-09-25 03:52:30 +0300

    added styling to topbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import Home from './pages/Home/Home';
-import { Person } from '@material-ui/icons'
 const App = () => {
-    return <Person />
+    return <Home />
 }
 
 export default App;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,8 @@
+import Topbar from "../../components/Topbar/Topbar";
+
 const Home = () => {
     return (
-        <div>
-            home page
-        </div>
+       <Topbar/>
     )
 }
 
