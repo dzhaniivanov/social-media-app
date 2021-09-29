edd8d7de33022ec4f9b7bef2186c82bf70fd2675
dzhani ivanov <djani.ivanov94@gmail.com>
2021-09-29 13:18:23 +0300

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,28 @@ import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
 import Register from './pages/Register/Register';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-    return <Home />
+
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/profile/:username">
+                    <Profile />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
