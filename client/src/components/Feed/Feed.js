fb76fc8181b3a9e5afbda5d71f09235da64fa071
dzhani ivanov <djani.ivanov94@gmail.com>
2021-10-02 16:16:01 +0300

    added axios

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,15 +1,29 @@
 import "./feed.css";
 import Share from "../Share/Share";
 import Post from "../Post/Post";
-import { Posts } from '../../dummyData';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
 const Feed = () => {
+    const [post, setPost] = useState([]);
+
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = axios.get("posts/timeline/61585891a5578fc5c68d121a");
+            console.log(res);
+        };
+        fetchPosts();
+    }, []);
+
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
-                {Posts.map((p) => (
+                {/*   {Posts.map((p) => (
                     <Post post={p} key={p.id} />
-                ))}
+                ))} */}
             </div>
         </div>
     )
--- client/src/components/Topbar/Topbar.js
+++ client/src/components/Topbar/Topbar.js
@@ -1,14 +1,19 @@
 import "./topbar.css";
-import {Search,Person,Chat,Notifications } from "@material-ui/icons";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+
 const Topbar = () => {
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
-                <span className="logo">SMedia</span>
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">SMedia</span>
+                </Link>
             </div>
             <div className="topbarCenter">
                 <div className="searchbar">
-                    <Search className="searchIcon"/>
+                    <Search className="searchIcon" />
                     <input placeholder="Search for friend,post or video" className="searchInput" />
                 </div>
             </div>
@@ -19,15 +24,15 @@ const Topbar = () => {
                 </div>
                 <div className="topbarIcons">
                     <div className="topbarIconItem">
-                        <Person/>
+                        <Person />
                         <span className="topbarIconBadge">1</span>
                     </div>
                     <div className="topbarIconItem">
-                        <Chat/>
+                        <Chat />
                         <span className="topbarIconBadge">2</span>
                     </div>
                     <div className="topbarIconItem">
-                        <Notifications/>
+                        <Notifications />
                         <span className="topbarIconBadge">1</span>
                     </div>
                 </div>
