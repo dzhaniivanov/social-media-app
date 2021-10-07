14e4e4dcbff394439f3d718359154f171a5a6b19
dzhani ivanov <djani.ivanov94@gmail.com>
2021-10-07 18:57:20 +0300

    work on login functionallity

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,15 +1,20 @@
+import { useContext, useRef } from "react";
 import "./login.css";
-import { useRef } from "react";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
 
     const email = useRef();
     const password = useRef();
+    const { user,isFetching, error, dispatch } = useContext(AuthContext);
 
     const handleClick = (e) => {
         e.preventDefault();
-        console.log(email);
-    }
+        loginCall({ email:email.current.value, password:password.current.value }, dispatch);
+    };
+
+    console.log(user);
 
 
 
